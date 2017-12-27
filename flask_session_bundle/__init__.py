5fdb98692084f1810fbb8ae3393122f69ac4c7af
Brian Cappello <briancappello@gmail.com>
2017-12-26 19:44:47 -0500

    add register_session_model_hook

--- flask_session_bundle/__init__.py
+++ flask_session_bundle/__init__.py
@@ -1,5 +1,8 @@
 from flask_unchained import Bundle
 
+from .register_session_model_hook import RegisterSessionModelHook
+
 
 class FlaskSessionBundle(Bundle):
     extensions_module_name = 'extension'
+    hooks = [RegisterSessionModelHook]
