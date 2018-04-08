700efcfb345cdab0f68d643e2e590b74f96e4321
Brian Cappello <briancappello@gmail.com>
2018-04-08 10:58:49 -0400

    release boilerplate

--- setup.py
+++ setup.py
@@ -1,22 +1,8 @@
-import os
-
-from codecs import open
 from setuptools import setup, find_packages
 
 
-ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
-with open(os.path.join(ROOT_DIR, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
-
-
-def read_requirements(filename):
-    def is_pkg(line):
-        return line and not line.startswith(('--', 'git', '#'))
-
-    with open(os.path.join(ROOT_DIR, filename), encoding='utf-8') as f:
-        return [line for line in f.read().splitlines() if is_pkg(line)]
 
 
 setup(
@@ -30,14 +16,19 @@ setup(
 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ],
-    packages=find_packages(),
-    install_requires=read_requirements('requirements.txt'),
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'flask-session>=0.3.1',
+        'flask-unchained>=0.2.0',
+    ],
     python_requires='>=3.6',
     include_package_data=True,
     zip_safe=False,
