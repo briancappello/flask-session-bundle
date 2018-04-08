eaac8d05af1cebba00c74317a7151afdd3aad402
Brian Cappello <briancappello@gmail.com>
2018-04-08 10:59:30 -0400

    release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='Flask Session Bundle',
-    version='0.1.0',
+    version='0.2.0',
     description='Adds server-side sessions support to Flask Unchained',
     long_description=long_description,
     url='https://github.com/briancappello/flask-session-bundle',
